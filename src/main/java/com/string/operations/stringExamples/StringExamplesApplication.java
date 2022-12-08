2cdbad7d13dff4f701a4cee7c52d3c6bb6d64e1f
kalyan <passiontechlearner@gmail.com>
2022-12-08 00:05:12 -0600

    just started the project

---
+++ src/main/java/com/string/operations/stringExamples/StringExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.string.operations.stringExamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/string/operations/stringExamples/StringExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.string.operations.stringExamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
